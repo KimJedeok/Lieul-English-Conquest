24659a213d5827ad7cd42f5d58e83c4116599f4f
KimJedeok <bachjd@gmail.com>
2026-08-21 23:14:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1302,7 +1302,7 @@ createApp({
             getMaskedWord, getMaskedExample, getHighlightedExampleMeaning, submitStage3MCQ, nextStage3Question,
             satStage, satWordIndex, satQuizList, satCorrectCount, feedbackMessage, isFeedbackCorrect, satComboCount, satCurrentWord,
             satInputText, submitSatAnswer, getWeakWords, startSaturdayReview, getMaskedSpelling20,
-            nextSatQuestion, satTimer, timerDisplay, satStageTitle, satStageThemeClass, satTotalQuestions, satHintText,
+            nextSatQuestion, satTimer, timerDisplay, satStageTitle, satStageThemeClass, satTotalQuestions, satHintText, restartSaturdayChallenge,
             isSatStageStarted, startSatStage,
             playTypingSound,
             startSatStage3Only,
